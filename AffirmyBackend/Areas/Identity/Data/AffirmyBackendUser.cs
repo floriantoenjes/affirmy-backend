f0f0836fc563926e39a7a41fccdec3dd6beeddeb
Florian Tönjes <skatepainter@yahoo.de>
2021-02-12 11:33:04 +0100

    Projektdateien hinzufügen.

---
+++ AffirmyBackend/Areas/Identity/Data/AffirmyBackendUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace AffirmyBackend.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the AffirmyBackendUser class
+    public class AffirmyBackendUser : IdentityUser
+    {
+    }
+}
---
+++ AffirmyBackend/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using AffirmyBackend.Areas.Identity.Data;
+using AffirmyBackend.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AffirmyBackend.Areas.Identity.IdentityHostingStartup))]
+namespace AffirmyBackend.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AffirmyBackendContext>(options =>
+                    options.UseSqlite(
+                        context.Configuration.GetConnectionString("AffirmyBackendContextConnection")));
+
+                services.AddDefaultIdentity<AffirmyBackendUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<AffirmyBackendContext>();
+            });
+        }
+    }
+}
