d51399286226661a7f949d92be1222cfed352bcb
Florian Tönjes <skatepainter@yahoo.de>
2021-02-12 12:31:26 +0100

    Implemented user registration and login

--- AffirmyBackend/Controllers/WeatherForecastController.cs
+++ AffirmyBackend/Controllers/WeatherForecastController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
+using AffirmyBackend.Areas.Identity.Data;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -18,10 +22,15 @@ namespace AffirmyBackend.Controllers
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
+        private readonly UserManager<AffirmyBackendUser> _userManager;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(
+            ILogger<WeatherForecastController> logger,
+            UserManager<AffirmyBackendUser> userManager
+        )
         {
             _logger = logger;
+            _userManager = userManager;
         }
 
         [HttpGet]
@@ -37,5 +46,66 @@ namespace AffirmyBackend.Controllers
             })
             .ToArray();
         }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterModel registerModel)
+        {
+            var userExists = await _userManager.FindByEmailAsync(registerModel.Email);
+            if (userExists != null)
+            {
+                return Forbid();
+            }
+            
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var newUser = new AffirmyBackendUser()
+            {
+                UserName = registerModel.Email,
+                Email = registerModel.Email,
+            };
+
+            var result = await _userManager.CreateAsync(newUser, registerModel.Password);
+            if (!result.Succeeded)
+            {
+                return Problem();
+            }
+
+            return Ok();
+        }
+        
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel loginModel)
+        {
+            var user = await _userManager.FindByEmailAsync(loginModel.Email);
+            if (user != null && await _userManager.CheckPasswordAsync(user, loginModel.Password))
+            {
+                return Ok();
+            }
+
+            return Unauthorized();
+        } 
+    }
+
+    public class RegisterModel
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+
+    public class LoginModel
+    {
+        [Required]
+        public string Email { get; set; }
+        
+        [Required]
+        public string Password { get; set; }
     }
 }
