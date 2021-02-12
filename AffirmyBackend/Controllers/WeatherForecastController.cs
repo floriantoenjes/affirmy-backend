05ebca2892399cc2674104e1e28c44c75318e246
Florian Tönjes <skatepainter@yahoo.de>
2021-02-12 12:38:38 +0100

    Added JWT creation on login

--- AffirmyBackend/Controllers/WeatherForecastController.cs
+++ AffirmyBackend/Controllers/WeatherForecastController.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using AffirmyBackend.Areas.Identity.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 
 namespace AffirmyBackend.Controllers
 {
@@ -23,14 +26,18 @@ namespace AffirmyBackend.Controllers
 
         private readonly ILogger<WeatherForecastController> _logger;
         private readonly UserManager<AffirmyBackendUser> _userManager;
+        private readonly IConfiguration _configuration;
 
         public WeatherForecastController(
             ILogger<WeatherForecastController> logger,
-            UserManager<AffirmyBackendUser> userManager
+            UserManager<AffirmyBackendUser> userManager,
+            IConfiguration configuration
         )
         {
             _logger = logger;
             _userManager = userManager;
+            _configuration = configuration;
+            
         }
 
         [HttpGet]
@@ -84,7 +91,19 @@ namespace AffirmyBackend.Controllers
             var user = await _userManager.FindByEmailAsync(loginModel.Email);
             if (user != null && await _userManager.CheckPasswordAsync(user, loginModel.Password))
             {
-                return Ok();
+                var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+
+                var token = new JwtSecurityToken(
+                    issuer: _configuration["JWT:ValidIssuer"],
+                    audience: _configuration["JWT:ValidAudience"],
+                    expires: DateTime.Now.AddHours(3),
+                    signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                );
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token),
+                    expiration = token.ValidTo
+                });
             }
 
             return Unauthorized();
